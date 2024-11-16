8a05453f640b89eab5de5b480ccc4726c9ea29f2
Robert Mads Christensen <61755754+madschristensen99@users.noreply.github.com>
2024-11-16 13:01:09 -0500

    Update WebProofVerifier.sol

--- contracts/scroll/WebProofVerifier.sol
+++ contracts/scroll/WebProofVerifier.sol
@@ -1,1 +1,15 @@
 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+import {EmailProver} from "./EmailProver.sol";
+import {Proof} from "vlayer-0.1.0/Proof.sol";
+import {Verifier} from "vlayer-0.1.0/Verifier.sol";
+contract EmailProofVerifier is Verifier {
+    address public prover;
+    constructor(address _prover) {
+        prover = _prover;
+    }
+    function verify(Proof calldata, uint clean, uint comms, uint rules) public view onlyVerified(prover, EmailProver.main.selector) {
+        //require(wallet == msg.sender, "Must be called from the same wallet as the registered address");
+    }
+}
